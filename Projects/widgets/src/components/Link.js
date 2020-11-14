e68bc6e7a6a23c698d9eaed2aa0362e5e911c8d7
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2020-11-14 15:55:08 -0800

    proj-widgets

---
+++ Projects/widgets/src/components/Link.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Link = ({ href, children, className }) => {
+  const onLinkClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, '', href);
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  };
+
+  return (
+    <a onClick={onLinkClick} className={className} href={href}>
+      {children}
+    </a>
+  );
+};
+
+export default Link;
