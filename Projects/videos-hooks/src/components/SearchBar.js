ee8af9c7eb37c0f1b503277a3afc2dd2cbd6933b
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2020-11-14 17:11:46 -0800

    proj-videos-hooks

---
+++ Projects/videos-hooks/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    onFormSubmit(term);
+  };
+
+  return (
+    <div className="ui segment search-bar">
+      <form action="" className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label htmlFor="">Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
